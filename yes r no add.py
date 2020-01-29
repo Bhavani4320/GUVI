153551de99fd8579c198181bc1ed2c8a2202a993
Bhavani4320 <60393083+Bhavani4320@users.noreply.github.com>
2020-01-29 15:48:07 +0530

    Add files via upload

---
+++ yes r no add.py
@@ -0,0 +1,8 @@
+ch="y"
+while(ch=="yes"or ch=="y"):
+  a=int (input("enter the value"))
+  b=int (input("enter the value"))
+  c=a+b
+  print("sum=",c)
+  print("do you want to continue:")
+  ch=input()
